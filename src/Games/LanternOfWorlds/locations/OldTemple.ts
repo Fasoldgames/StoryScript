a728716ebc15b229e06cec0cd0ad6b05a4f7c3b5
Fabian <info@fasoldgames.com>
2026-02-08 09:48:25 +0100

    Added new action buttons.

--- src/Games/LanternOfWorlds/locations/OldTemple.ts
+++ src/Games/LanternOfWorlds/locations/OldTemple.ts
@@ -3,16 +3,16 @@ import { OldPillar } from './OldPillar';
 import description from './OldTemple.html?raw';
 import { Start } from './start';
 import { StrangeStructure } from './StrangeStructure';
+import { ActionStatus } from '../../../Engine/Interfaces/enumerations/actionStatus';
 
 export function OldTemple() {
+	let hasLookedAround = false;
+
 	return Location({
 		name: 'Old ruins',
 		description: description,
 		destinations: [
-			{
-				name: 'Return to Camp',
-				target: Start
-			},	
+			
 			{
 				name: 'Old Pillar',
 				target: OldPillar
@@ -36,10 +36,42 @@ export function OldTemple() {
 			'Look',
 				{
 				text: 'Look around',
+				status: () => hasLookedAround ? ActionStatus.Unavailable : ActionStatus.Available,
 				execute: (game: IGame) => {
 					game.currentLocation.descriptionSelector='look';
 					game.actionLog.length=0;
+					hasLookedAround = true;
 					return true;
+				},
+			}
+		],[
+			'InvestigatePillar',
+				{
+				text: 'Investigate the pillar',
+				status: () => hasLookedAround ? ActionStatus.Available : ActionStatus.Unavailable,
+				execute: (game: IGame) => {
+					game.changeLocation(OldPillar, true);
+					return false;
+				},
+			}
+		],[
+			'InvestigateTomb',
+				{
+				text: 'Investigate the tomb',
+				status: () => hasLookedAround ? ActionStatus.Available : ActionStatus.Unavailable,
+				execute: (game: IGame) => {
+					game.actionLog.push('You investigate the mysterious tomb.');
+					return true;
+				},
+			}
+		],[
+			'Return to Camp',
+				{
+				text: 'Return to Camp',
+				execute: (game: IGame) => {
+					game.changeLocation(Start, true);
+					game.currentLocation.descriptionSelector = 'returned';
+					return false;
 				},
 			}
 		],
